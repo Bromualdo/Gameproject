cf44cd64035308aee444008a33a327e65bc4fb28
Adrian Pozzi <adrianpozzi@gmail.com>
2023-03-18 23:57:33 -0300

    terminadas las clases de personajes

--- primeraentrega.js
+++ primeraentrega.js
@@ -1,5 +1,104 @@
 // D&D , simulador de creacion de personaje //
 
+class Mago{
+    constructor(nombre,fuerza,inteligencia,carisma){
+    this.nombre=nombre
+    this.fuerza=parseInt(fuerza)
+    this.inteligencia=parseInt(inteligencia)
+    this.carisma=parseInt(carisma)
+       
+    let habilidades =[ {
+        Nombre:"Misil Magico",
+        Danio:10}
+    ,{Nombre:"Bola de Fuego",
+    Danio:20},
+    {Nombre:"Saeta de hielo"
+    ,Danio:15}]
+
+    this.daniohabilidad= function(number){
+        
+        let daniototal=habilidades[number].Danio*inteligencia/5
+        return daniototal
+    }
+    this.habilidadUsada = function(number) {
+        return habilidades[number].Nombre
+    }
+    this.aprenderHabilidad= function(ob){
+        return habilidades.push(ob)
+    }
+    this.showhabilidades=function(){
+        return habilidades
+    }
+}
+
+}
+
+class Guerrero{
+    constructor(nombre,fuerza,inteligencia,carisma){
+    this.nombre=nombre
+    this.fuerza=parseInt(fuerza)
+    this.inteligencia=parseInt(inteligencia)
+    this.carisma=parseInt(carisma)
+    
+    let habilidades =[ {
+        Nombre:"Golpe Brutal",
+        Danio:20}
+    ,{Nombre:"Golpe Desafiante",
+    Danio:20},
+    {Nombre:"Cien Filos"
+    ,Danio:15}]
+
+    this.daniohabilidad= function(number){
+        
+        let daniototal=habilidades[number].Danio*fuerza/5
+        return daniototal
+    }
+    this.habilidadUsada = function(number) {
+        return habilidades[number].Nombre
+    }
+    this.aprenderHabilidad= function(ob){
+        return habilidades.push(ob)
+    }
+    this.showhabilidades=function(){
+        return habilidades
+    }
+}
+
+}
+
+class Bardo{
+    constructor(nombre,fuerza,inteligencia,carisma){
+    this.nombre=nombre
+    this.fuerza=parseInt(fuerza)
+    this.inteligencia=parseInt(inteligencia)
+    this.carisma=parseInt(carisma)
+    
+    let habilidades =[ {
+        Nombre:"La llamada del viajero",
+        Danio:10}
+    ,{Nombre:"Notas Punzantes",
+    Danio:20},
+    {Nombre:"Instrumento desafinado"
+    ,Danio:15}]
+
+    this.daniohabilidad= function(number){
+        
+        let daniototal=habilidades[number].Danio*carisma/5
+        return daniototal
+    }
+    this.habilidadUsada = function(number) {
+        return habilidades[number].Nombre
+    }
+    this.aprenderHabilidad= function(ob){
+        return habilidades.push(ob)
+    }
+    this.showhabilidades=function(){
+        return habilidades
+    }
+}
+
+}
+
 const valorAleatorio = (min,max) =>
 {
 min = Math.ceil(min)
@@ -11,28 +110,29 @@ let nombre=prompt('que nombre tendra el personaje? (minimo 3 caracteres)')
 let fuerza=parseInt(0)
 let carisma=parseInt(0)
 let inteligencia=parseInt(0)
-let vida=parseInt(100)
-let vidaenemigo=parseInt(100)
+let vida=100
+let vidaenemigo=100
 let confirmaPersonaje=false
-let cantidadDeRounds=parseInt(valorAleatorio(1,6))
-let iniciativaJugador=parseInt(0)
-let iniciativaEnemigo=parseInt(0)
-let golpeTuyo=parseInt(0)
-let golpeEnemigo=parseInt(0)
-let dañoTotalTuyo=parseInt(0)
-let dañoTotalEnemigo=parseInt(0)
+let cantidadDeRounds=valorAleatorio(1,6)
+let iniciativaJugador=0
+let iniciativaEnemigo=0
+let golpeTuyo=0
+let golpeEnemigo=0
+let dañoTotalTuyo=0
+let dañoTotalEnemigo=0
+let eleccion=''
 
 
 
 const Hit = () =>{
-    let acerto =false
+    let Acerto =false
     
     if (valorAleatorio(0,10) >5){
         
-        return acerto=true
+        return Acerto=true
     }
     else{
-        return acerto=false
+        return Acerto=false
     }
     
     
@@ -41,39 +141,45 @@ const Hit = () =>{
 if ((nombre.length >= 3) &&(nombre.length <=12 ) ) {
     
     do {
-    let clase=prompt('que clase vas a elegir?? (Bardo,Mago,Guerrero)') // <-- que clase vas a elegir?
+    let eleccion=prompt('que clase vas a elegir?? (Bardo,Mago,Guerrero)') // <-- que clase vas a elegir?
 
     
 
  
     // -- asignacion de estadisticas -- //
-    switch (clase) {   
+    switch (eleccion) {   
         case 'Bardo':
-            fuerza=valorAleatorio(1,5)
-            carisma=valorAleatorio(5,10)
-            inteligencia=valorAleatorio(1,8)
+            
+            // fuerza=valorAleatorio(1,5)
+            // carisma=valorAleatorio(5,10)
+            // inteligencia=valorAleatorio(1,8)
+            clase=new Bardo(nombre,valorAleatorio(1,5),valorAleatorio(1,8),valorAleatorio(5,10))
             
             break 
         case 'Guerrero':
-            fuerza=valorAleatorio(5,10)
-            carisma=valorAleatorio(1,8)
-            inteligencia=valorAleatorio(1,5)
             
+            // fuerza=valorAleatorio(5,10)
+            // carisma=valorAleatorio(1,8)
+            // inteligencia=valorAleatorio(1,5)
+            clase=new Guerrero(nombre,valorAleatorio(5,10),valorAleatorio(1,5),valorAleatorio(1,8))
+
             break
         case 'Mago':
-            fuerza=valorAleatorio(1,5)
-            carisma=valorAleatorio(1,8)
-            inteligencia=valorAleatorio(5,10)      
             
+            // fuerza=valorAleatorio(1,5)
+            // carisma=valorAleatorio(1,8)
+            // inteligencia=valorAleatorio(5,10)      
+            clase=new Mago(nombre,valorAleatorio(1,5),valorAleatorio(5,10),valorAleatorio(1,8))
             break
         default:
             alert('hubo un error entra otra vez')
             break
     }
 
-    
-    confirmaPersonaje= confirm('Tu nombre es '+nombre+' y la clase elegida es '+Clase+' y los stats son:'+'\nFuerza: '+Fuerza+'\nCarisma: '+Carisma+'\nInteligencia: '+Inteligencia+'\nEstas de acuerdo o volvemos a empezar?')
+   
+    confirmaPersonaje= confirm('Tu nombre es '+clase.nombre+' y la clase elegida es '+eleccion+' y los stats son:'+'\nFuerza: '+clase.fuerza+'\nCarisma: '+clase.carisma+'\nInteligencia: '+clase.inteligencia+'\nEstas de acuerdo o volvemos a empezar?')
     } while (confirmaPersonaje==false)
+   
 }
 else{
 alert('hubo un error ingresando el nombre , volve a empezar')
@@ -83,13 +189,16 @@ alert('te encuentras con un enemigo que esta dispuesto a pelear! la pelea durara
 
 
 for (let i=1; i<=cantidadDeRounds; i++ ){
-    // iniciativaJugador=valorAleatorio(0,10)
-    // iniciativaEnemigo=valorAleatorio(0,10)
+    iniciativaJugador=valorAleatorio(0,10)
+    iniciativaEnemigo=valorAleatorio(0,10)
    
     if (Hit()===false) {
-        golpeTuyo=valorAleatorio(1,15)
-        alert('Aciertas!\nTu daño es de: '+golpeTuyo)
-        vidaenemigo-=golpeTuyo
+        // golpeTuyo=valorAleatorio(1,15)
+        usoDeHabilidad=valorAleatorio(0,2)
+        danioHabilidad=clase.daniohabilidad(1)
+        habilidadUsada=clase.habilidadUsada(usoDeHabilidad)
+        alert('Aciertas!\nUtilizas '+habilidadUsada+'!!\nTu daño es de: '+danioHabilidad)
+        vidaenemigo-=danioHabilidad
     }
     else {
         golpeEnemigo=valorAleatorio(1,15)
